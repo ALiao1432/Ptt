7fadd4780ea6855b55ed61d14c7e193257fb4cc3
ALiao1432 <apple36578@gmail.com>
2019-05-10 13:42:14 +0800

    fix article not load bug

--- app/src/main/java/study/ian/ptt/fragment/ArticleFragment.java
+++ app/src/main/java/study/ian/ptt/fragment/ArticleFragment.java
@@ -335,7 +335,6 @@ public class ArticleFragment extends BaseFragment
         currentOffset = "";
         currentOffsetSig = "";
 
-        disposables.dispose();
         disposables.clear();
         articleAdapter.clearResults();
     }
